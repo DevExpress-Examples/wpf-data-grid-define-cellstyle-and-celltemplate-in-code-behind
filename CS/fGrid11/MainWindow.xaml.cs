1edce34c3bc9f938a0f225eb7da3b0c8d81a8aad
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/fGrid11/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.Grid;
+using DevExpress.Xpf.Grid.Themes;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace fGrid11 {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void AddCellTemplateButton_Click_1(object sender, RoutedEventArgs e) {
+            string ageBinding = "IsReadOnly=\"{Binding RowData.EvenRow}\"";
+            string nameBinding = "FontSize=\"{Binding RowData.Row.NameFontSize}\"";
+            DataTemplate ageCellDataTemplate = CreateDataTemplate(ageBinding);
+            if (ageCellDataTemplate != null)
+                gridControl1.Columns["Age"].CellTemplate = ageCellDataTemplate;
+            DataTemplate nameCellDataTemplate = CreateDataTemplate(nameBinding);
+            if (nameCellDataTemplate != null)
+                gridControl1.Columns["LastName"].CellTemplate = nameCellDataTemplate;
+        }
+
+        private void AddStyleButton_Click_1(object sender, RoutedEventArgs e) {
+            Style style = new Style();
+            Style baseStyle = FindResource(new GridRowThemeKeyExtension() { ResourceKey = GridRowThemeKeys.LightweightCellStyle, ThemeName = ThemeManager.ApplicationThemeName }) as Style;
+            style.BasedOn = baseStyle;
+            style.TargetType = typeof(LightweightCellEditor);
+            style.Setters.Add(new Setter(LightweightCellEditor.BackgroundProperty, new SolidColorBrush(Colors.LightGreen)));
+            gridControl1.Columns["LastName"].CellStyle = style;
+        }
+
+        string myCellTemplate = "<DataTemplate xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\" " +
+            "xmlns:dxe=\"http://schemas.devexpress.com/winfx/2008/xaml/editors\">" +
+            "<dxe:TextEdit Name=\"PART_Editor\" HorizontalContentAlignment=\"Center\" REPLACE/></DataTemplate>";
+
+        DataTemplate CreateDataTemplate(string markupPart) {
+            string template = myCellTemplate.Replace("REPLACE", markupPart);
+            DataTemplate dataTemplate = XamlReader.Load(GetStreamFromString(template)) as DataTemplate;
+            return dataTemplate;
+        }
+
+        public static Stream GetStreamFromString(string s) {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
---
+++ CS/fGrid11/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace fGrid11
+{
+    public class Person
+    {
+        public Person(int i) {
+            LastName = "LastName #" + i;
+            Age = i * 10;
+        }
+        public static ObservableCollection<Person> CreateList {
+            get {
+                ObservableCollection<Person> list = new ObservableCollection<Person>();
+                for (int i = 0; i < 10; i++)
+                    list.Add(new Person(i) { NameFontSize = 12 + (i % 6) });
+                return list;
+            }
+        }
+        public int Age { get; set; }
+        public string LastName { get; set; }
+        public double NameFontSize { get; set; }
+    }
+}
